3122c433433ee242f4c09c360606041d8ab6282c
Alexsander Alves de Oliveira <106824625+sanderalves1@users.noreply.github.com>
2022-06-26 23:14:39 -0300

    S3-11 Javascript: Praticando com Variáveis
    M13 - Alexsander Alves de Oliveira - S3-11 Javascript: Praticando com Variáveis

---
+++ script.js
@@ -0,0 +1,13 @@
+let resultado = 0
+let nome = "Alexsander"
+let sobrenome = "Alves de Oliveira"
+let anoNascimento = "1971"
+let anoAtual = "2022"
+let cidade = "Rio de Janeiro"
+resultado = anoAtual - anoNascimento
+    // console.log(resultado)
+
+// console.log(`${nome} ${sobrenome}`)
+
+
+console.log("Eu sou o " + `${nome} ${sobrenome} moro na cidade do ${cidade} e tenho ${resultado} de idade`)
